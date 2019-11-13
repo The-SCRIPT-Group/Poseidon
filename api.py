f82ec169e22b07b5645db9e143273833bf5146e6
Akhil Narang <akhilnarang.1999@gmail.com>
2019-11-13 16:39:20 +0530

    erp: whole lotta changes and cleanups
    Extra requirements added because pandas seems high
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- api.py
+++ api.py
@@ -1,13 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 
-from erp import attendance
+from erp import attendance, attendance_json
 
-app = Flask('script erp scraper')
+app = Flask(__name__)
 
 
-@app.route('/attendance', methods=['GET'])
+@app.route('/api/attendance', methods=['POST'])
 def get_attendance():
-    return attendance(request.args.get('username'), request.args.get('password'))
+    return attendance_json(request.form['username'], request.form['password'])
+
+
+@app.route('/attendance', methods=['GET', 'POST'])
+def view_attendance():
+    if request.method == 'POST':
+        return attendance(request.form['username'], request.form['password'])
+    return """
+        <form action="" method="POST">
+            <p><input type="text" name="username" required>
+            <p><input type="password" name="password" required>
+            <p><input type="submit" value="Login">
+        </form>
+        """
+
+
+@app.route('/')
+def root():
+    return redirect(url_for('attendance'))
 
 
 if __name__ == "__main__":
--- erp.py
+++ erp.py
@@ -9,65 +9,50 @@ import requests
 from pandas import read_html
 
 payload = {
-    "__LASTFOCUS": "",
     "__VIEWSTATE": "/wEPDwULLTE5ODI5MDAxMzMPFgIeDkxPR0lOX0JBU0VEX09OZRYCAgEPZBYCAgMPZBYCZg9kFgYCDw9kFgICAQ8QZA8WAWYWARAFA1dQVQUDV1BVZxYBZmQCEw8PFgIeB0VuYWJsZWRnZGQCGQ9kFgICAQ8QZGQWAWZkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQhjaGtDaGVja+/IxmsP3IoCwjVYbsmN45kfOjGivX4s7e93RISZwDsW",
     "__EVENTTARGET": "btnLogin",
-    "__EVENTARGUMENT": "",
     "chkCheck": "on",
     "__VIEWSTATEGENERATOR": "B8B84CAE",
-    "hdnMsg": "",
-    "hdtype": "",
-    "hdloginid": "",
-    "hdnFlag": "R",
 }
-
-headers = {"Content-Type": "application/json; charset=utf-8"}
 
 
 def attendance(username, password):
     captcha_file = str(uuid4().hex) + ".png"
-    boo = True
 
     with requests.session() as s:
-        while boo:
-            try:
-                data = s.post(
-                    "https://erp.mitwpu.edu.in/AdminLogin.aspx/funGenerateCaptcha",
-                    headers=headers,
-                ).text
-                img = loads(data)["d"]
-                with open(captcha_file, "wb") as captcha:
-                    captcha.write(b64decode(img))
-                captcha_text = loki.image_to_string(captcha_file,
-                                                    config="--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789abcdef")
-                if captcha_text == '':
-                    raise ValueError()
-                print(f"Captcha is {captcha_text}")
-
-                payload['txtUserId'] = username
-                payload["txtPassword"] = password
-                payload["txtCaptcha"] = captcha_text
-
-                # headers for logging in
-                headers["X-MicrosoftAjax"] = "Delta=true"
-                headers["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8"
-                headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " \
-                                        "Chrome/77.0.3865.120 Safari/537.36"
-                # api call to log in
-                response = s.post(
-                    "https://erp.mitwpu.edu.in/AdminLogin.aspx", headers=headers, data=payload
-                )
-                print(response)
-                table = read_html(s.get(
-                    "https://erp.mitwpu.edu.in/STUDENT/SelfAttendence.aspx?MENU_CODE=MWEBSTUATTEN_SLF_ATTEN"
-                ).text)[0]
-
-                boo = False
-
-            except ValueError:
-                print('retrying...')
-
+        captcha_text = ""
+        while captcha_text == "":
+            headers = {"Content-Type": "application/json; charset=utf-8"}
+            data = s.post(
+                "https://erp.mitwpu.edu.in/AdminLogin.aspx/funGenerateCaptcha",
+                headers=headers,
+            ).text
+            img = loads(data)["d"]
+            with open(captcha_file, "wb") as captcha:
+                captcha.write(b64decode(img))
+            captcha_text = loki.image_to_string(captcha_file,
+                                                config="--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789abcdef")
         os.remove(captcha_file)
+
+        payload['txtUserId'] = username
+        payload["txtPassword"] = password
+        payload["txtCaptcha"] = captcha_text
+
+        # Headers for logging in and fetching the data
+        headers["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8"
+        headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                                "Chrome/77.0.3865.120 Safari/537.36"
+        # POST request to log in
+        s.post(
+            "https://erp.mitwpu.edu.in/AdminLogin.aspx", headers=headers, data=payload
+        )
+        return s.get(
+            "https://erp.mitwpu.edu.in/STUDENT/SelfAttendence.aspx?MENU_CODE=MWEBSTUATTEN_SLF_ATTEN"
+        ).text
+
+
+def attendance_json(username, password):
+    table = read_html(attendance(username, password))[0]
 
     data = list()
 
