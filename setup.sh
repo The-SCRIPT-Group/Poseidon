e101897c9c26ac438ce59bd6a1d69ccc96da0409
Akhil Narang <akhilnarang.1999@gmail.com>
2019-12-05 17:14:29 +0530

    Poseidon: no more heroku
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2154
+
+export DEBIAN_FRONTEND="noninteractive"
+sudo apt update -y
+sudo apt install python3.8* git nginx python-certbot-nginx python3-pip tesseract-ocr tesseract-ocr-eng -y
+cd /tmp || exit
+sudo certbot --noninteractive --nginx --agree-tos --email akhilnarang@thescriptgroup.in --domain poseidon.thescriptgroup.in
+cat << EOF | sudo tee /etc/nginx/sites-available/poseidon.thescriptgroup.in
+server {
+    listen 80;
+    server_name poseidon.thescriptgroup.in;
+    location ^~ /.well-known/acme-challenge/ {
+        root /var/www/html;
+    }
+    location / {
+        return 301 https://poseidon.thescriptgroup.in$request_uri;
+    }
+}
+
+server {
+    listen 443 ssl;
+    server_name poseidon.thescriptgroup.in;
+    ssl_certificate /etc/letsencrypt/live/poseidon.thescriptgroup.in/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/poseidon.thescriptgroup.in/privkey.pem;
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+    location ^~ / {
+        proxy_pass        http://127.0.0.1:5501;
+    }
+}
+EOF
+sudo ln -s /etc/nginx/sites-available/poseidon.thescriptgroup.in /etc/nginx/sites-enabled/poseidon.thescriptgroup.in
+sudo rm -fv /etc/nginx/sites-{available,enabled}/default
+sudo nginx -s reload
+echo '30 2 * * * /usr/bin/certbot renew --noninteractive --renew-hook "/usr/sbin/nginx -s reload" >> /var/log/le-renew.log' > /tmp/cron
+sudo crontab /tmp/cron
+rm -v /tmp/cron
+cd - || exit
+git clone https://github.com/The-SCRIPT-Group/poseidon.git
+cd poseidon || exit
+pip3 install -r requirements.txt
+echo "Setup your configuration file and run the application! (make sure its running on port 5000, should be the default with waitress)"
