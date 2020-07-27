5d4385dfad2052fa46d27ba62059baf6548880ec
Akhil Narang <me@akhilnarang.dev>
2020-07-28 00:19:59 +0530

    Poseidon: update setup
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- setup.sh
+++ setup.sh
@@ -6,17 +6,12 @@ export DEBIAN_FRONTEND="noninteractive"
 sudo apt update -y
 sudo apt install python3.8* git nginx python-certbot-nginx python3-pip tesseract-ocr tesseract-ocr-eng -y
 cd /tmp || exit
-sudo certbot --noninteractive --nginx --agree-tos --email akhilnarang@thescriptgroup.in --domain poseidon.thescriptgroup.in
+sudo certbot --noninteractive --webroot --webroot-path /var/www/html --agree-tos --email akhilnarang@thescriptgroup.in --domain poseidon.thescriptgroup.in
 cat << EOF | sudo tee /etc/nginx/sites-available/poseidon.thescriptgroup.in
 server {
     listen 80;
     server_name poseidon.thescriptgroup.in;
-    location ^~ /.well-known/acme-challenge/ {
-        root /var/www/html;
-    }
-    location / {
-        return 301 https://poseidon.thescriptgroup.in;
-    }
+    return 301 https://poseidon.thescriptgroup.in;
 }
 
 server {
@@ -27,12 +22,18 @@ server {
     include /etc/letsencrypt/options-ssl-nginx.conf;
     ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
+    location ^~ /.well-known/acme-challenge/ {
+        root /var/www/html;
+    }
+
     location /static {
         root /home/akhil/Poseidon;
         try_files $uri $uri/ =404;
     }
 
     location ^~ / {
+        add_header \'Access-Control-Allow-Origin\' \'*\';
+
         proxy_pass        http://127.0.0.1:5501;
         proxy_redirect    off;
 
@@ -48,11 +49,3 @@ EOF
 sudo ln -s /etc/nginx/sites-available/poseidon.thescriptgroup.in /etc/nginx/sites-enabled/poseidon.thescriptgroup.in
 sudo rm -fv /etc/nginx/sites-{available,enabled}/default
 sudo nginx -s reload
-echo '30 2 * * * /usr/bin/certbot renew --noninteractive --renew-hook "/usr/sbin/nginx -s reload" >> /var/log/le-renew.log' > /tmp/cron
-sudo crontab /tmp/cron
-rm -v /tmp/cron
-cd - || exit
-git clone https://github.com/The-SCRIPT-Group/poseidon.git
-cd poseidon || exit
-pip3 install -r requirements.txt
-echo "Setup your configuration file and run the application! (make sure its running on port 5000, should be the default with waitress)"
